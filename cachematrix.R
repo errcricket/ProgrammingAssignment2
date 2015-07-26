7e75fc4a9470df1c9677f57caf4bc820dffc6065
reichenberger <cricket@reichenbergers-MacBook-Pro.local>
2015-07-26 17:21:40 -0400

    Made makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+#1. Use this function to create a special "matrix" object that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix()) 
+{
+	seq_matrix <<- matrix(rnorm(16), 4)
+	print(seq_matrix)
 }
 
+makeCacheMatrix()
 
-## Write a short comment describing this function
+#2. Use this function to calculate the inverse matrix
+cacheSolve <- function(x, ...) 
+{
+ 	if (det(x) != 0)
+	{
+		invMatrix <<- solve(x) 
+	}
+}
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve(seq_matrix)
+print(invMatrix)
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
